dae11349e4a5f128a51259d14a5ee54f1055db1c
Camille Hall <73563880+camihall@users.noreply.github.com>
2021-06-25 12:10:44 -0500

    heres code for how we discussed to format the csv
    I didn't get it to start at 0. the name of each column starts at 9 as does the index. So I was wrong in the meeting sorry. Starts at 9 not 0. (but it is only the questions as discussed)

---
+++ formatingCSV.py
@@ -0,0 +1,11 @@
+#this is just to specify which columns to use. Its kinda wordy but gets the job done
+cols = []
+for i in range(9,129):
+    cols.append(i)
+
+#For now just download to your computer and use that pathway or figure out how to download from google sheets.
+dataf = pd.read_csv(r'C:\users\camih\documents\NASA_Centennial_Challenges_Survey.csv', skiprows = [0,1], header = None, usecols = cols) 
+questions = pd.read_csv(r'C:\users\camih\documents\NASA_Centennial_Challenges_Survey.csv', nrows=1, header = None, usecols = cols)
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+questions
+dataf
