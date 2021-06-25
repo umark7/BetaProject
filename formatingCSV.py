91d896cfdc92b0a84a2028b0b8c59ebf1eed0c56
Camille Hall <73563880+camihall@users.noreply.github.com>
2021-06-25 12:31:45 -0500

    Update formatingCSV.py
    changed nrows from 1 to 2 for questions

--- formatingCSV.py
+++ formatingCSV.py
@@ -5,7 +5,7 @@ for i in range(9,129):
 
 #For now just download to your computer and use that pathway or figure out how to download from google sheets.
 dataf = pd.read_csv(r'C:\users\camih\documents\NASA_Centennial_Challenges_Survey.csv', skiprows = [0,1], header = None, usecols = cols) 
-questions = pd.read_csv(r'C:\users\camih\documents\NASA_Centennial_Challenges_Survey.csv', nrows=1, header = None, usecols = cols)
+questions = pd.read_csv(r'C:\users\camih\documents\NASA_Centennial_Challenges_Survey.csv', nrows=2, header = None, usecols = cols)
 pd.set_option("display.max_rows", None, "display.max_columns", None)
 questions
 dataf
